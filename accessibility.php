2ae6be31d9ba700a5bdbc1642ca70495754345b6
Ben Morrison <ben@flyht.uk>
2023-10-26 16:44:15 +0100

    Add content and images

--- accessibility.php
+++ accessibility.php
@@ -13,7 +13,7 @@
 <body data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0">
   <?php include "partials/header.php" ?>
   <main id="content">
-    <section class="hero d-flex justify-content-center flex-column relative flexible-content home-image">
+    <section class="hero d-flex justify-content-center flex-column relative flexible-content fc-margin-bottom standard-image coloured_intro_block red">
       <span class="bg-push-top cover">
         <span class="cover d-block d-md-none  bg-image-cover"
           style="background-image: url();"></span>
@@ -27,10 +27,23 @@
       </span>
       <div class="container position-relative z-index-2 ">
         <div class="col-12 col-lg-10 no-padding text-wrapping no-padding ">
-          <h1 class="title pad-bottom-20-l pad-bottom-10 no-margin">Accessibility Information</h1>
+          <h1 class="title pad-bottom-20-l pad-bottom-10 no-margin">Accessibility</h1>
         </div>
       </div>
     </section>
+    <div class="flexible-content-container">
+      <section class="standard_text flexible-content fc-padding-bottom ">
+        <div class="container d-block align-items-center">
+          <div class="text-wrapper ">
+            <div class="desc">
+              <p>As we hire space to meet we aim to thoroughly consider accessibility. We try to provide a variety of ways to participate on a Sunday to be enable people to be themselves. There is often an activity table with mindful colouring and crafts, and we like to offer a quieter space to be. We don’t expect anyone to stay seated in rows. We also try to let you know what kind of practices are on offer each week and what will be covered so you can anticipate the activities and message. </p>
+              <p>At our gatherings there will be gluten free snack options and oat and cow’s milk on offer for hot drinks.</p>
+              <p>We can always do better and are committed to reflecting on our practice as we develop ways of meeting together and doing good stuff. Do let us know if there is anything we can help with to enable you to ‘come as you are’.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </main>
   <?php include "partials/footer.php" ?>
   <?php include "partials/scripts.php" ?>
