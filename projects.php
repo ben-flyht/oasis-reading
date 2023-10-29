c6b4efb1a540f13a48727aa15795ebc87a119681
Ben Morrison <ben@flyht.uk>
2023-10-29 15:31:56 +0000

    Change projects hero image

--- projects.php
+++ projects.php
@@ -16,13 +16,13 @@
     <section class="hero d-flex justify-content-center flex-column relative flexible-content fc-margin-bottom standard-image">
       <span class="bg-push-top cover">
         <span class="cover d-block d-md-none  bg-image-cover"
-          style="background-image: url(./assets/sign-post.jpg);"></span>
+          style="background-image: url(./assets/heart-in-hands.jpg);"></span>
         <span class="cover d-none d-md-block d-lg-none  bg-image-cover"
-          style="background-image: url(./assets/sign-post.jpg);"></span>
+          style="background-image: url(./assets/heart-in-hands.jpg);"></span>
         <span class="cover d-none d-lg-block d-xl-none  bg-image-cover"
-          style="background-image: url(./assets/sign-post.jpg);"></span>
+          style="background-image: url(./assets/heart-in-hands.jpg);"></span>
         <span class="cover d-none d-xl-block  bg-image-cover"
-          style="background-image: url(./assets/sign-post.jpg);"></span>
+          style="background-image: url(./assets/heart-in-hands.jpg);"></span>
         <span class="header-overlay cover z-index-1"></span>
       </span>
       <div class="container position-relative z-index-2 ">
