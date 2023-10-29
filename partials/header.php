ce8caea0b090b3f9cab62042a57d1a5a762a3759
Ben Morrison <ben@flyht.uk>
2023-10-29 15:32:11 +0000

    Remove safeguarding from main nav

--- partials/header.php
+++ partials/header.php
@@ -5,7 +5,6 @@
       <li id="menu-item-556" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-556"><a href="./about.html">About Us</a></li>
       <li id="menu-item-551" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-551"><a href="./gatherings.html">Gatherings</a></li>
       <li id="menu-item-554" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-554"><a href="./projects.html">Projects</a></li>
-      <li id="menu-item-553" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-553"><a href="./safeguarding.html">Safeguarding</a></li>
       <li id="menu-item-555" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-555"><a href="./contact.html">Contact</a></li>
       <li id="menu-item-1077" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1077"><a href="./donate.html">Donate</a></li>
     </ul>
@@ -23,7 +22,6 @@
           <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-556 <?php if ($CURRENT_PAGE == "about") echo "current-menu-item current_page_item" ?>"><a href="./about.html">About Us</a></li>
           <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-551 <?php if ($CURRENT_PAGE == "gatherings") echo "current-menu-item current_page_item" ?>"><a href="./gatherings.html">Gatherings</a></li>
           <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-554 <?php if ($CURRENT_PAGE == "projects") echo "current-menu-item current_page_item" ?>"><a href="./projects.html">Projects</a></li>
-          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-553 <?php if ($CURRENT_PAGE == "safeguarding") echo "current-menu-item current_page_item" ?>"><a href="./safeguarding.html">Safeguarding</a></li>
           <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-555 <?php if ($CURRENT_PAGE == "contact") echo "current-menu-item current_page_item" ?>"><a href="./contact.html">Contact</a></li>
           <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1077"><a href="./donate.html">Donate</a></li>
         </ul>
